462c5f0a063778e59effc51a39bc74bb7bea42ff
DTU054 <90808309+DTU054@users.noreply.github.com>
2023-08-04 18:10:57 -0400

    dddd

--- sketch.js
+++ sketch.js
@@ -11,25 +11,61 @@ var amplitude;
 var bassVibrate = true;
 let counter = 0;
 
+var hu = 0; // Hue for stroke color
+var sat = 100; // Saturation for stroke color
+var brt = 100; // Brightness for stroke color
+var alph = 10; // Alpha for stroke color
+
 $(document).ready(function () {
-    // Check for vibration support
-    navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
-
-    // Determine if vibration is supported in this web browser
-    if (!navigator.vibrate) {
-        $('#supported').hide();
-        return;
-    }
-
-    $('#unsupported').hide();
-
-    const musicFileInput = document.getElementById('musicFile');
-    musicFileInput.addEventListener('change', onMusicSelected);
-
-    fft = new p5.FFT();
-    soundPlaying = false;
+    // Rest of the code
 });
 
+function setup() {
+    createCanvas(windowWidth - 20, windowHeight - 20);
+    // Rest of the code
+}
+
+function draw() {
+    if (soundPlaying) {
+        let spectrum = fft.analyze();
+        var x1 = width * noise(t + 15);
+        var x2 = width * noise(t + 25);
+        var x3 = width * noise(t + 35);
+        var x4 = width * noise(t + 45);
+        var y1 = height * noise(t + 55);
+        var y2 = height * noise(t + 65);
+        var y3 = height * noise(t + 75);
+        var y4 = height * noise(t + 85);
+
+        bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+
+        let rms = amplitude.getLevel();
+        t += rms/10;
+
+        if (frameCount % 500 == 0) {
+            background(255);
+        }
+    }
+}
+
+function unhighlight() {
+    introP.style('color', 'white');
+}
+
+function highlight() {
+    introP.style('color', 'blue');
+}
+
+function gotFile(file) {
+    sound = loadSound(file.data, onMusicLoaded, fileError);
+    fft = new p5.FFT();
+    sound.amp(0.2);
+
+    // create a new Amplitude analyzer
+    amplitude = new p5.Amplitude();
+    // Patch the input to a volume analyzer
+    amplitude.setInput(sound);
+}
 
 function onMusicSelected(event) {
     const file = event.target.files[0];
@@ -59,84 +95,4 @@ function musicFinishedPlaying() {
     soundPlaying = false;
     // Stop vibration when music finishes playing
     navigator.vibrate(0);
-}
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    stroke(0, 20);
-    noFill();
-    t = 0;
-
-    introP = createP("");
-    introP.style("color", "#fff");
-    introP.position(width / 1, height / 1);
-
-    uploadMusic = select('#uploadMusicHere');
-    uploadMusic.dragOver(highlight);
-    uploadMusic.dragLeave(unhighlight);
-    uploadMusic.drop(gotFile, unhighlight);
-
-    amplitude = new p5.Amplitude();
-
-}
-
-function draw() {
-    if (soundPlaying) {
-        let spectrum = fft.analyze();
-        var x1 = width * noise(t + 15);
-        var x2 = width * noise(t + 25);
-        var x3 = width * noise(t + 35);
-        var x4 = width * noise(t + 45);
-        var y1 = height * noise(t + 55);
-        var y2 = height * noise(t + 65);
-        var y3 = height * noise(t + 75);
-        var y4 = height * noise(t + 85);
-
-        bezier(x1, y1, x2, y2, x3, y3, x4, y4);
-
-        let rms = amplitude.getLevel();
-        t += rms/10;
-
-        if (frameCount % 500 == 0) {
-            background(255);
-        }
-
-
-
-        // if(rms > 0.1 && bassVibrate){
-        //     navigator.vibrate(200);
-        //     //console.log(rms)
-        //     bassVibrate = false;
-        // }
-        // else {
-        //     bassVibrate = true;
-        // }
-    }
-}
-
-function unhighlight() {
-    introP.style('color', 'white');
-}
-
-function highlight() {
-    introP.style('color', 'blue');
-}
-
-function gotFile(file) {
-    sound = loadSound(file.data, onMusicLoaded, fileError);
-    fft = new p5.FFT();
-    sound.amp(0.2);
-
-    // create a new Amplitude analyzer
-    amplitude = new p5.Amplitude();
-    // Patch the input to an volume analyzer
-    amplitude.setInput(sound);
-}
-
-function fileSuccess() {
-    introP.hide();
-    sound.play();
-    soundPlaying = true;
-    console.log("sound file uploaded");
-    
 }
